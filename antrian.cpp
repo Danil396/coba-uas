e8c8120f7fd5f4537195d5b202f163415f3cebf7
triyana2503010068 <2503010068@unper.ac.id>
2026-06-18 15:35:14 +0700

    Menambahkan file antrian.cpp dan memodifikasi file main.cpp

--- antrian.cpp
+++ antrian.cpp
@@ -1,12 +1,81 @@
-#include <iostream>
+#include<iostream>
+#include<queue>
+#include<iomanip>
 using namespace std;
 
-void menuantrian();
-void tambahantrian();
-void keluarantrian();
+struct Kendaraan{
+    string platNomor, jenis; //mobil atau motor
+};
 
-int main()
-{
+//tambah antrian
+void tambahAntrian(queue<Kendaraan>&a, Kendaraan data){
+    a.push(data);
+    cout<<"Kendaraan "<<data.platNomor<<"( "<<data.jenis<<" ) berhasil ditambahkan.\n";
+}
 
-    return 0;
+//tampil antrian
+void tampilAntrian(queue<Kendaraan>a){
+    if(a.empty()){
+        cout<<"Antrian kosong!\n";
+        return;
+    }
+    cout << "+----+----------------+-----------------+\n";
+    cout << "| " << left << setw(3)  << "NO" 
+         << "| " << setw(15) << "Plat Nomor" 
+         << "| " << setw(15) << "Jenis Kendaraan" << " |\n";
+    cout << "+----+----------------+-----------------+\n";
+    
+    int no = 1;
+    while(!a.empty()){
+        Kendaraan k = a.front();
+        cout<<"| "<<left<<setw(3)<<no
+            <<"| "<<setw(15)<<k.platNomor
+            <<"| "<<setw(15)<<k.jenis<<" |\n";
+        a.pop();
+        no++;
+    }
+    cout << "+----+----------------+-----------------+\n";
+}
+
+void menuantrian(){
+    queue<Kendaraan>antrian;
+    int pilihan;
+
+    do{
+        cout << "\n===== MENU ANTRIAN =====\n";
+        cout << "=1. Tambah Antrian      =\n";
+        cout << "=2. Tampil Antrian      =\n";
+        cout << "=0. Kembali Program     =\n";
+        cout << "=Pilihan: ";
+        cin >> pilihan;
+
+        switch (pilihan){
+            case 1:{
+                Kendaraan data;
+                cout<<"Masukan Plat Nomor: ";
+                cin.ignore();
+                getline(cin, data.platNomor);
+
+                int jenisPilihan;
+                cout<<"Jenis kendaraan ( 1 = Mobil, 2  = Motor): ";
+                cin>>jenisPilihan;
+
+                if(jenisPilihan == 1)data.jenis = " Mobil ";
+                else if(jenisPilihan == 2)data.jenis = " Motor ";
+                else data.jenis = "Tidak diketahui!";
+
+                tambahAntrian(antrian, data);
+                break;
+            }
+            case 2:
+                tampilAntrian(antrian);
+                break;
+            case 0:
+                cout<<"Kembali ke program utama.\n";
+                break;
+            default:
+                cout<<"Pilihan tidak valid!\n";
+        }
+    }
+    while(pilihan != 0);
 }
--- main.cpp
+++ main.cpp
@@ -1,32 +1,33 @@
 #include<iostream>
 using namespace std;
 
-#include "slot.cpp"
+
 #include "antrian.cpp"
-#include "riwayat.cpp"
+
 
 int main()
 {
-
     int pilihan;
     do
     {
         cout << "\n============= SISTEM SMART PARKING ==============\n";
-        cout << "1.Data Antrian\n";
-        cout << "2.Data Slot Parkir Antrian\n";
-        cout << "3.Data Riwayat  Parkir\n";
+        cout << "1.Menu Slot\n";
+        cout << "2.Menu antrian\n";
+        cout << "3.Menu Riwayat  Parkir\n";
         cout << "0.Keluar ...\n";
+        cout << "Pilihan: ";
+        cin >> pilihan;          // <-- ini yang kurang
 
         switch (pilihan)
         {
         case 1:
-            menuslot();
+            // menuslot();
             break;
         case 2:
             menuantrian();
             break;
         case 3:
-            menuriwayat();
+            // menuriwayat();
             break;
         case 0:
             cout << "Keluar ..\n";
